daa570c7d5c5311cfc63d9d6292ae462282ed1e7
cawfree <hello@cawfree.com>
2022-11-03 02:39:25 +0000

    chore: proof verification

--- scripts/witness.ts
+++ scripts/witness.ts
@@ -4,6 +4,9 @@ import * as os from 'os';
 import * as path from 'path';
 import * as fs from 'fs';
 
+// @ts-ignore
+import * as snarkjs from 'snarkjs';
+
 import {nanoid} from 'nanoid';
 import * as child_process from "child_process";
 
@@ -35,6 +38,17 @@ void (async () => {
     {stdio: 'inherit', cwd: path.resolve('build')},
   );
 
+  const verificationKey = path.resolve('build', `${circuit}_verification_key.json`);
+
+  const res = await snarkjs.groth16.verify(
+    JSON.parse(fs.readFileSync(verificationKey, 'utf-8')),
+    JSON.parse(fs.readFileSync(pub, 'utf-8')),
+    JSON.parse(fs.readFileSync(proof, 'utf-8')),
+  );
+
+  const success = res === true;
+
+  console.log(`Verification ${success ? 'Succeeded' : 'Failed'} ${success ? '✅' : '🚫'}`);
 })();
 
 
