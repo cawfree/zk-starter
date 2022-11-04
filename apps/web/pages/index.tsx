c4282b300712062855dcb7c11b26589ffe2c8c5c
cawfree <hello@cawfree.com>
2022-11-04 02:35:48 +0000

    chore: rainbowkit

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,46 @@
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+    ConnectButton,
+    getDefaultWallets,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+    chain,
+    configureChains,
+    createClient,
+    WagmiConfig,
+} from 'wagmi';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+const { chains, provider } = configureChains(
+  [chain.localhost],
+  [
+    jsonRpcProvider({
+      rpc: () => ({
+        http: 'http://localhost:8545',
+      }),
+    }),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'zk-starter',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider
+});
+
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <ConnectButton />
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
