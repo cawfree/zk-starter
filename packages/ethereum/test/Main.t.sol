ed899ebd0308378f8c0ba7a66755bbfeaeba1d36
cawfree <hello@cawfree.com>
2022-11-04 00:18:24 +0000

    chore: rename Main.sol

---
+++ packages/ethereum/test/Main.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Main.sol";
+
+contract MainTest is Test {
+    Main public main;
+
+    function setUp() public {
+        main = new Main();
+        main.setNumber(0);
+    }
+
+    function testIncrement() public {
+        main.increment();
+        assertEq(main.number(), 1);
+    }
+
+    function testSetNumber(uint256 x) public {
+        main.setNumber(x);
+        assertEq(main.number(), x);
+    }
+}
