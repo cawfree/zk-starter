6dd2352e41a287667abd9ece98c6620238502882
cawfree <hello@cawfree.com>
2022-11-05 20:08:23 +0000

    chore: docs

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -92,13 +92,10 @@ export default function Main(): JSX.Element {
             We can use adversarial incentivised networks like <a href="https://ethereum.org">Ethereum</a> to perform computation with very strong guarantees of correctness, immutability and verifiability.
           </p>
           <p>
-            Although the security and resilience of public blockchains are indeed thanks to their open nature, humans nonetheless have a fundamental right to privacy, and decentralized autonomous organizations must be capable of privileged non-invasive handling of secure data.
+            Although the security and resilience of public blockchains are indeed thanks to their open nature, humans nonetheless have a fundamental right to privacy. Similarly, increasing competition for block space and the subsequent rise in gas fees demand that we take more computation and data off-chain without compromising on our stringent operational expectations.
           </p>
           <p>
-            A zero knowledge proof is a goldilocks paradigm, an inexplicable feature of the mathematics of the universe; it helps us all operate privately, securely, trustlessly, with integrity; all via publicly-verifiable information channels.
-          </p>
-          <p>
-            The ability to securely and indisputably demonstrate access to information without revealing that information is magical.
+            A zero knowledge proof is a goldilocks paradigm, an inexplicable feature of the mathematics of the universe which helps us all operate privately, securely, trustlessly, minimally, with integrity; and above all else, in plain sight.
           </p>
           {isConnected && (
             <button
